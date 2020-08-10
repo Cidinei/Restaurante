2f163a3bbebc3a2aad71291c4f64f603d23a010f
Cidinei <cidineijulho@gmail.com>
2020-08-10 14:09:35 -0300

    Atualizção de inteface

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,10 @@ var express = require("express");
 var router = express.Router();
 var users = require("./../inc/users");
 
-router.use(function(req, res, next){
-  if (['/login'].indexOf(req.url) === -1 && !req.session.user){
+router.use(function (req, res, next) {
+  if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
     res.redirect("/admin/login");
-  } else{
+  } else {
     next();
   }
 });
@@ -17,11 +17,12 @@ router.get('/logout', function (req, res, next) {
 
 router.get('/', function (req, res, next) {
   res.render("admin/index", {
-    title: 'ADM - CPANEL',
+    title: 'ADMIN - CPANEL',
     h1: 'ADM - CPANEL',
   });
 
 });
+
 router.get('/login', function (req, res, next) {
   users.render(req, res, null);
 });
@@ -45,11 +46,9 @@ router.post('/login', function (req, res, next) {
 
 });
 
-
-
 router.get('/contacts', function (req, res, next) {
   res.render("admin/contacts", {
-    title: 'Contatos',
+    title: 'ADMIN - CPANEL',
     h1: 'ADM - CPANEL',
   });
 
@@ -57,37 +56,35 @@ router.get('/contacts', function (req, res, next) {
 
 router.get('/menus', function (req, res, next) {
   res.render("admin/menus", {
-    title: 'Menus',
-    h1: 'CPANEL',
+    title: 'ADMIN - CPANEL',
+    h1: 'ADM - CPANEL',
   });
 
 });
 
 router.get('/emails', function (req, res, next) {
   res.render("admin/emails", {
-    title: 'admin',
-    h1: 'CPANEL',
+    title: 'ADMIN - CPANEL',
+    h1: 'ADM - CPANEL',
   });
 
 });
 
 router.get('/users', function (req, res, next) {
   res.render("admin/users", {
-    title: 'admin',
-    h1: 'CPANEL',
+    title: 'ADMIN - CPANEL',
+    h1: 'ADM - CPANEL',
   });
 
 });
+
 router.get('/reservations', function (req, res, next) {
   res.render("admin/reservations", {
     date: '09-08-2020',
-    title: 'admin',
-    h1: 'CPANEL',
+    title: 'ADMIN - CPANEL',
+    h1: 'ADM - CPANEL',
   });
 
 });
-
-
-
 
 module.exports = router;
